19e314eff9fca7bda005d9a8b16ed6fbd3db1104
Nurkholis <nurkholiswakhid@gmail.com>
2024-10-27 23:21:36 +0700

    Initial commit

--- app/src/main/java/com/dicodingeventstracker/domain/Splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/dicodingeventstracker/domain/Splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,12 @@
-package com.dicodingeventstracker.domain.Splashscreen
+package com.dicodingeventstracker.domain.splashscreen
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.dicodingeventstracker.R
 import com.dicodingeventstracker.domain.home.MainActivity
 import java.util.Timer
@@ -18,8 +18,34 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // Set status bar appearance based on theme
+        setupStatusBar()
+
+        // Navigate to MainActivity after delay
+        setupNavigation()
+    }
+
+    private fun setupStatusBar() {
+        val isDarkMode = resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        window.statusBarColor = if (isDarkMode) {
+            getColor(R.color.black)
+        } else {
+            getColor(R.color.white)
+        }
+
+        // Get the WindowInsetsControllerCompat
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+        // Configure the behavior of the status bars
+        windowInsetsController.isAppearanceLightStatusBars = !isDarkMode
+    }
+
+    private fun setupNavigation() {
         Timer("splashGone", true).schedule(3000) {
-            Intent(this@SplashScreenActivity, MainActivity::class.java).also { backToMain->
+            Intent(this@SplashScreenActivity, MainActivity::class.java).also { backToMain ->
                 startActivity(backToMain)
                 finish()
             }
